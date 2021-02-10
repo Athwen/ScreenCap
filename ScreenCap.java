fca8bb0bf4149add183dbdecef53bb42a5ff372b
Alexander Mayorga <alexmayorga@knights.ucf.edu>
2021-02-10 09:11:57 -0500

    Gets screencapture

--- ScreenCap.java
+++ ScreenCap.java
@@ -1,7 +1,36 @@
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import java.awt.Rectangle;
+
 class ScreenCap{
 
     public static void main(String args[]){
+
+        try{
+            Robot robit = new Robot();
+
+            Rectangle rect = new Rectangle(0, 0, 1920, 1080);
+
+            BufferedImage buff = robit.createScreenCapture(rect);
+
+            File output = new File("image.jpg");
+
+            ImageIO.write(buff, "jpg", output);
+            
+
+
+
+        }catch(Exception err){
+            System.out.println(err);
+
+        }
         
+
+
 
 
     }
